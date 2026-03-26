b4a6b4f05a084c6559e83980188ea2281e966dff
gabriel correa <gabriel2pk@hotmail.com>
2026-03-26 18:15:51 -0400

    fix: move Firebase credentials to environment variables

---
+++ app.config.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'BankIo',
+    slug: 'bankio',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './assets/android-icon-foreground.png',
+        backgroundImage: './assets/android-icon-background.png',
+        monochromeImage: './assets/android-icon-monochrome.png',
+      },
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+  },
+};
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,14 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import Constants from 'expo-constants';
+
+const extra = Constants.expoConfig?.extra ?? {};
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCA6kqctYoqi9tQ_Qmy7a4lQbzOrTBLr60',
-  authDomain: 'grimorio-416b6.firebaseapp.com',
-  projectId: 'grimorio-416b6',
-  storageBucket: 'grimorio-416b6.firebasestorage.app',
-  messagingSenderId: '556724769927',
-  appId: '1:556724769927:web:7f4cb60f4fcdedb11cd422',
+  apiKey: extra.firebaseApiKey,
+  authDomain: extra.firebaseAuthDomain,
+  projectId: extra.firebaseProjectId,
+  storageBucket: extra.firebaseStorageBucket,
+  messagingSenderId: extra.firebaseMessagingSenderId,
+  appId: extra.firebaseAppId,
 };
 
 const app = initializeApp(firebaseConfig);
